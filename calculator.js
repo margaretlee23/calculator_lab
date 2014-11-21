459d3f95ae1d93aeb0c73aad783dc9e09958f6e0
Margaret Lee <Margaret.Mi.Lee@gmail.com>
2014-11-21 10:18:43 -0800

    Updates from yesterday

---
+++ calculator.js
@@ -0,0 +1,19 @@
+
+
+document.querySelector("#buttons_row_1").children[0].addEventListener("click", function() {
+	  alert("1");
+	});
+document.querySelector("#buttons_row_1").children[1].addEventListener("click", function() {
+	  alert("1");
+	});
+document.querySelector("#buttons_row_1").children[2].addEventListener("click", function() {
+	  alert("1");
+	});
+
+document.querySelector("#buttons_row_1").children[3].addEventListener("click", function() {
+	  alert("1");
+	});
+
+document.querySelector("#buttons_row_1").children[4].addEventListener("click", function() {
+	  alert("1");
+	});
